868171beeca418a93c212377c246e2521a484538
Getlan <annelyse.nugue@gmail.com>
2016-10-06 22:43:42 +0200

    Main controllers + views creation
    Created the 5 main controllers and views for : index, login, fighter, sight, diary.

---
+++ src/Controller/ArenasController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+/**
+* Personal Controller
+* User personal interface
+*
+*/
+class ArenasController  extends AppController
+{
+public function index()
+{
+}
+
+public function login()
+{
+}
+
+public function fighter()
+{
+}
+
+public function sight()
+{
+}
+
+public function diary()
+{
+}
+}
